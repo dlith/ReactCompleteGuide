7dcbf8aac05bca00f8a5eb4f524ae869d8ec091e
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-10-22 17:16:23 +0200

    import & export

---
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,8 @@
+//import { apiKey, apiKey2 as value } from "./util";
+//import key from "./util";
+
+import * as util from "./util";
+
+console.log(util.apiKey);
+console.log(util.default);
+console.log(util.apiKey2);
---
+++ 00-starting-js-proj/assets/scripts/util.js
@@ -0,0 +1,3 @@
+export let apiKey = "never gonna give you up";
+export default "never gonna let you down";
+export let apiKey2 = "Never gonna run around and desert you";
