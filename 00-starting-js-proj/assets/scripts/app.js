93922716b139787c260b8b7aa0047a4e1e69b658
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-10-22 18:41:28 +0200

    Variables and functions

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -6,3 +6,19 @@ import * as util from "./util";
 console.log(util.apiKey);
 console.log(util.default);
 console.log(util.apiKey2);
+
+
+let userMessage = "Rickrolled";
+const constMessage = "never gonna give, never gonna give";
+console.log(userMessage);
+console.log(constMessage);
+
+
+function greet(message, defaultMessage = "Rickroll") {
+  //console.log(message);
+  //console.log(defaultMessage);
+  return defaultMessage + ": " + message; 
+}
+
+console.log(greet(util.apiKey));
+console.log(greet(util.apiKey2));
