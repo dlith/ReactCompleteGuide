c1d63564803ce6b209fde4847ba0c926fda1199d
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-10-26 13:08:39 +0200

    The special 'children' prop

--- 01-react-essntials/src/App.jsx
+++ 01-react-essntials/src/App.jsx
@@ -1,7 +1,8 @@
 import componentsImg from "./assets/components.png";
-import { CORE_CONCEPTS } from "./data";
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
+import {CORE_CONCEPTS} from "./data";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -12,11 +13,24 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {/* <CoreConcept title="Component" description='The core UI building block' image={componentsImg} /> */}
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
         <h2>Time to get started!</h2>
       </main>
