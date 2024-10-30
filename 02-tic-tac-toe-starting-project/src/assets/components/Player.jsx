0fe34a93c98aa0e6343d707abb647768b6ca2690
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-10-30 15:44:56 +0100

    working with state for player name

--- 02-tic-tac-toe-starting-project/src/assets/components/Player.jsx
+++ 02-tic-tac-toe-starting-project/src/assets/components/Player.jsx
@@ -1,11 +1,26 @@
+import {useState} from "react";
+
 export default function Player({name, symbol}) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing(true);
+  }
+
+  let playername;
+  if (isEditing) {
+    playername = <input type="text" className="player-name" required/>;
+  } else {
+    playername = <span className="player-name">{name}</span>;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playername}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>Edit</button>
     </li>
   );
 }
