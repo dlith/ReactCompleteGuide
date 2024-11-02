b3c9cb2f6bea649756d983a2e7fbe5c15b926be2
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-11-02 13:22:34 +0100

    lifting state up

--- 03-investment-calculator/src/App.jsx
+++ 03-investment-calculator/src/App.jsx
@@ -1,11 +1,32 @@
 import Header from "./components/Header";
+import Results from "./components/Results";
 import UserInput from "./components/UserInput";
+import {useState} from "react";
+
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 12000,
+    annualInvestment: 1000,
+    expectedReturn: 5.5,
+    duration: 12,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
   );
 }
