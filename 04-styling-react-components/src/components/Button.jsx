9609ae0a7ad10a197b2375e3dbf576258f6164d3
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-11-09 13:34:28 +0100

    Migrating the demo app to tailwind CSS

--- 04-styling-react-components/src/components/Button.jsx
+++ 04-styling-react-components/src/components/Button.jsx
@@ -20,7 +20,7 @@
 export default function Button({children, ...props}) {
   return (
     <button
-      className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bd-amber-400 hover:bg-amber-500"
+      className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
       {...props}
     >
       {children}
