d13765a563fb0bd877eeef99174faa1de4dbd321
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-11-17 17:22:37 +0100

    Closing the dialog tag after pressed ESC key

--- 06-refs-portals-project/src/components/ResultModule.jsx
+++ 06-refs-portals-project/src/components/ResultModule.jsx
@@ -15,7 +15,7 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime, remaini
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You {result}</h2>}
       {!userLost && <h2>Your score: {score}</h2>}
       <p>
