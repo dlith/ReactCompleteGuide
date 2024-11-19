e0be25ee7aca8a73e5af1c7efd372e50c4c8ced9
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-11-19 19:22:59 +0100

    dding a projects sidebar component

---
+++ 07-project-management/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add project</button>
+      </div>
+      <ul>...</ul>
+    </aside>
+  );
+}
