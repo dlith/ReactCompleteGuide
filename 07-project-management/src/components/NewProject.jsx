6e004174303d2f0963dce1b0bfb6f35650eae73d
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-11-26 16:30:20 +0100

    Validation user input and showing an error modal via useImperativeHandle

--- 07-project-management/src/components/NewProject.jsx
+++ 07-project-management/src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import Input from "./Input.jsx";
 import {useRef} from "react";
+import Modal from "./Modal.jsx";
 
 export default function NewProject({onAdd}) {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +13,10 @@ export default function NewProject({onAdd}) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    //validation...
+    if (enteredTitle.trim() === "" || enteredDescription.trim() === "" || enteredDueDate.trim() === "") {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -22,6 +27,11 @@ export default function NewProject({onAdd}) {
 
   return (
     <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Input</h2>
+        <p>Oops ... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
