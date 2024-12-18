6b0148d842f164caa3781a31aee5b504b5046c89
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-12-18 19:49:11 +0100

    preparing another problem that can be fixed with useEffect

--- 09-placepicker-project/src/components/DeleteConfirmation.jsx
+++ 09-placepicker-project/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,7 @@
 export default function DeleteConfirmation({onConfirm, onCancel}) {
+  console.log("Timer set");
+  setTimeout(() => {}, 3000);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
