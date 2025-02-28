43b0ba70f63cfb14acfdcf3a37088d0b59ffb651
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-02-28 19:44:45 +0100

    managing and getting user input via state and generic hanglers

--- 15-react-forms/src/components/Login.jsx
+++ 15-react-forms/src/components/Login.jsx
@@ -1,7 +1,31 @@
+import {useState} from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Submitted!");
+    console.log(enteredValues);
+  }
+
+  // function hangleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function hanglePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
+  function hangleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
   }
 
   return (
@@ -11,12 +35,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => hangleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => hangleInputChange("password", event.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
