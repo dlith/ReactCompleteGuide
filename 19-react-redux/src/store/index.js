01d2200a912b0623668016f1c68bebb202cc7217
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-06-14 14:55:25 +0200

    creating a redux sotre for react

---
+++ 19-react-redux/src/store/index.js
@@ -0,0 +1,15 @@
+import {createStore} from "redux";
+
+const counterReducer = (state = {count: 0}, action) => {
+  if (action.type === "increment") {
+    return {...state, count: state.count + 1};
+  }
+
+  if (action.type === "decrement") {
+    return {...state, count: state.count - 1};
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
