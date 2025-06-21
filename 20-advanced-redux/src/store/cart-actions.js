7e8cf63975a2cad612069e6623b89de2b35609f4
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-06-21 12:07:53 +0200

    finalizing the fetching logic

--- 20-advanced-redux/src/store/cart-actions.js
+++ 20-advanced-redux/src/store/cart-actions.js
@@ -18,7 +18,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -46,7 +51,7 @@ export const sendCartData = (cart) => {
         "https://advanced-redux-fe377-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({items: cart.items, totalQuantity: cart.totalQuantity}),
         }
       );
 
