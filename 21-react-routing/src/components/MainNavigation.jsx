f1432782c8061ac93e5703d4604c34fd284f0522
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-06-28 12:48:03 +0200

    adding relative and absolute paths

--- 21-react-routing/src/components/MainNavigation.jsx
+++ 21-react-routing/src/components/MainNavigation.jsx
@@ -7,12 +7,12 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <NavLink to={"/"} className={({isActive}) => (isActive ? classes.active : undefined)} end>
+            <NavLink to={""} className={({isActive}) => (isActive ? classes.active : undefined)} end>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to={"/products"} className={({isActive}) => (isActive ? classes.active : undefined)}>
+            <NavLink to={"products"} className={({isActive}) => (isActive ? classes.active : undefined)}>
               Products
             </NavLink>
           </li>
