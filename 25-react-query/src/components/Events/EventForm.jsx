8a2ff7e50f1ade9e2549cff6c72c0a7716cc64fe
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-08-04 19:07:40 +0200

    fetching more data and testing the mutation

--- 25-react-query/src/components/Events/EventForm.jsx
+++ 25-react-query/src/components/Events/EventForm.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import {useState} from "react";
+import {useQuery} from "@tanstack/react-query";
 
-import ImagePicker from '../ImagePicker.jsx';
+import ImagePicker from "../ImagePicker.jsx";
+import {fetchSelectableImages} from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
-export default function EventForm({ inputData, onSubmit, children }) {
+export default function EventForm({inputData, onSubmit, children}) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
+
+  const {data, isPending, isError} = useQuery({
+    queryKey: ["events-images"],
+    queryFn: fetchSelectableImages,
+  });
 
   function handleSelectImage(image) {
     setSelectedImage(image);
@@ -15,68 +23,44 @@ export default function EventForm({ inputData, onSubmit, children }) {
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
 
-    onSubmit({ ...data, image: selectedImage });
+    onSubmit({...data, image: selectedImage});
   }
 
   return (
     <form id="event-form" onSubmit={handleSubmit}>
       <p className="control">
         <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          defaultValue={inputData?.title ?? ''}
-        />
+        <input type="text" id="title" name="title" defaultValue={inputData?.title ?? ""} />
       </p>
 
-      <div className="control">
-        <ImagePicker
-          images={[]}
-          onSelect={handleSelectImage}
-          selectedImage={selectedImage}
-        />
-      </div>
+      {isPending && <p>Loading selectable images...</p>}
+      {isError && <ErrorBlock title="Filaed to load selectable images" message="Please try again later"></ErrorBlock>}
+      {data && (
+        <div className="control">
+          <ImagePicker images={data} onSelect={handleSelectImage} selectedImage={selectedImage} />
+        </div>
+      )}
 
       <p className="control">
         <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          name="description"
-          defaultValue={inputData?.description ?? ''}
-        />
+        <textarea id="description" name="description" defaultValue={inputData?.description ?? ""} />
       </p>
 
       <div className="controls-row">
         <p className="control">
           <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            defaultValue={inputData?.date ?? ''}
-          />
+          <input type="date" id="date" name="date" defaultValue={inputData?.date ?? ""} />
         </p>
 
         <p className="control">
           <label htmlFor="time">Time</label>
-          <input
-            type="time"
-            id="time"
-            name="time"
-            defaultValue={inputData?.time ?? ''}
-          />
+          <input type="time" id="time" name="time" defaultValue={inputData?.time ?? ""} />
         </p>
       </div>
 
       <p className="control">
         <label htmlFor="location">Location</label>
-        <input
-          type="text"
-          id="location"
-          name="location"
-          defaultValue={inputData?.location ?? ''}
-        />
+        <input type="text" id="location" name="location" defaultValue={inputData?.location ?? ""} />
       </p>
 
       <p className="form-actions">{children}</p>
