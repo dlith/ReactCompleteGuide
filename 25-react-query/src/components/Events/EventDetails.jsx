edeef7307ebc1919f724ab41084cde42f4c4179d
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-08-06 19:08:45 +0200

    add delete event details functionality with mutation

--- 25-react-query/src/components/Events/EventDetails.jsx
+++ 25-react-query/src/components/Events/EventDetails.jsx
@@ -1,17 +1,33 @@
-import {Link, Outlet, useParams} from "react-router-dom";
+import {Link, Outlet, useParams, useNavigate} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
-import {fetchEvent} from "../../util/http.js";
+import {deleteEvent, fetchEvent} from "../../util/http.js";
+import {useMutation} from "@tanstack/react-query";
 
 import Header from "../Header.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
-import EventItem from "./EventItem.jsx";
+import {queryClient} from "../../util/http.js";
 
 export default function EventDetails() {
+  const navigate = useNavigate();
   const params = useParams();
   const {data, isPending, isError, error} = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({signal}) => fetchEvent({id: params.id, signal}),
   });
+
+  const {mutate} = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+      });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({id: params.id});
+  }
 
   let content;
 
@@ -47,7 +63,7 @@ export default function EventDetails() {
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
